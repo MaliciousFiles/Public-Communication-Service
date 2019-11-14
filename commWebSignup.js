4e7dcf448959a5dcd7e2d49640328f0a55b0bc72
Malcolm07 <57738870+Malcolm07@users.noreply.github.com>
2019-11-13 21:20:24 -0600

    V1.2

--- commWebSignup.js
+++ commWebSignup.js
@@ -1,11 +1,10 @@
-//location.protocol="http"
-require(['fs'], function (fs) {});
+const fs=require('fs')
 function checkForEntry() {
-	/*fs.readFile('users.txt', (err, data) => {
+	fs.readFile('users.txt', (err, data) => {
 		if (err) throw err;
 		
 		console.log(data.toString());
-	});*/
+	});
 	var rect=(document.getElementById('input').getBoundingClientRect());
 	if (document.getElementById('password').value!=document.getElementById('passwordCheck').value) {
 		document.getElementById('passwordNotSameError').innerHTML = "Passwords don't match!"
