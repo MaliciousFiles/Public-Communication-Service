34fd6395d93aeca93619fffbd05b2afaca490561
Malcolm07 <57738870+Malcolm07@users.noreply.github.com>
2019-11-14 21:25:28 -0600

    2.1

--- commWebSignup.js
+++ commWebSignup.js
@@ -3,11 +3,9 @@ function checkForEntry() {
         var username = (snapshot.val() && snapshot.val().username) || 'Anonymous';
         // ...
     }));
-	var rect=(document.getElementById('input').getBoundingClientRect());
 	if (document.getElementById('password').value!=document.getElementById('passwordCheck').value) {
 		document.getElementById('passwordNotSameError').innerHTML = "Passwords don't match!"
 	} else {
 		document.getElementById('passwordNotSameError').innerHTML = ""
 	};
-	//if (document.getElementById('username').value in readFile(./users.txt)
 };
