99d44dce1e011bd3ce644824961695c39df1dd10
Malcolm07 <57738870+Malcolm07@users.noreply.github.com>
2019-11-14 21:08:35 -0600

    2.1

--- commWebSignup.js
+++ commWebSignup.js
@@ -1,5 +1,5 @@
 function checkForEntry() {
-	saveToFirebase(document.getElementById('username').value)
+	
 	var rect=(document.getElementById('input').getBoundingClientRect());
 	if (document.getElementById('password').value!=document.getElementById('passwordCheck').value) {
 		document.getElementById('passwordNotSameError').innerHTML = "Passwords don't match!"
