9d6e7b5e3c7bf7b176f1c66f2e37f639727784a9
Malcolm07 <57738870+Malcolm07@users.noreply.github.com>
2019-11-14 21:23:50 -0600

    2.1

--- commWebSignup.js
+++ commWebSignup.js
@@ -1,5 +1,8 @@
 function checkForEntry() {
-	
+	return firebase.database().ref('/' + document.getElementById('username').value).once('value').then(function(snapshot) {
+        var username = (snapshot.val() && snapshot.val().username) || 'Anonymous';
+        // ...
+    });
 	var rect=(document.getElementById('input').getBoundingClientRect());
 	if (document.getElementById('password').value!=document.getElementById('passwordCheck').value) {
 		document.getElementById('passwordNotSameError').innerHTML = "Passwords don't match!"
