6d04c57d841a1b0492dadb9c9892226715156b95
Malcolm07 <57738870+Malcolm07@users.noreply.github.com>
2019-11-15 10:02:07 -0600

    2.3.1

--- commWebSignup.js
+++ commWebSignup.js
@@ -1,11 +1,36 @@
 function checkForEntry() {
-	var ref = firebase.database().ref('/'+document.getElementById('username').value);
-    ref.on("value", function(snapshot) {
-        console.log(snapshot.val());
-    });
-	if (document.getElementById('password').value!=document.getElementById('passwordCheck').value) {
-		document.getElementById('passwordNotSameError').innerHTML = "Passwords don't match!"
+	//Start checks for username field
+	//look for username
+	var userCheck = firebase.database().ref('/'+document.getElementById('username').value);
+	ref.on("value", function(snapshot) {
+		console.log(snapshot.val());
+	});
+	//if statements
+	var userField = False
+	if (document.getElementById('username').value==null) {
+		document.getElementById('usernameError').innerHTML = "The username field can't be blank!"
+	} else if (userCheck!=null) {
+		document.getElementById('usernameError').innerHTML = "That username already exists!"
 	} else {
-		document.getElementById('passwordNotSameError').innerHTML = ""
+		userField = True
+	};
+	//start checks for first name field
+	//look for first name
+	var userCheck = firebase.database().ref('/'+document.getElementById('username').value+document.getElementById('firstName').value);
+	ref.on("value", function(snapshot) {
+		console.log(snapshot.val());
+	});
+	//if statements
+	if (document.getElementById('firstName').value==null) {
+		document.getElementById('usernameError').innerHTML = "The username field can't be blank!"
+	} else if (userCheck!=null) {
+		document.getElementById('usernameError').innerHTML = "That username already exists!"
+	} else {
+		userField = True
+	};
+	if (document.getElementById('password').value!=document.getElementById('passwordCheck').value) {
+		document.getElementById('passwordCheckError').innerHTML = "Passwords don't match!"
+	} else {
+		document.getElementById('passwordCheckError').innerHTML = ""
 	};
 };
