5593c6148415427c2ff95dc037e8bd4ce8044144
Malcolm07 <57738870+Malcolm07@users.noreply.github.com>
2019-11-19 19:34:15 -0600

    1.3

--- home.js
+++ home.js
@@ -1,5 +1,5 @@
 window.onscroll = function() {
-  if windowpageYOffset >= 1165.5px {
+  if windowpageYOffset >= 1166 {
     document.getElementById('navBar').style.top = "0px";
   } else {
     document.getElementById('navBar').style.top = "-50px";
