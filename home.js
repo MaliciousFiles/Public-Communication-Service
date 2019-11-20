61fbb32ab531649552dafef315f03c95f6fe88ca
Malcolm07 <57738870+Malcolm07@users.noreply.github.com>
2019-11-20 08:19:31 -0600

    2.3

--- home.js
+++ home.js
@@ -10,5 +10,7 @@ function sleep (time) {
   return new Promise((resolve) => setTimeout(resolve, time));
 }
 sleep(12000).then(() => {
-    window.scrollBy(0,500)
+    if window.pageYOffset < 500 {
+      window.scrollTo(0,500);
+    };
 });
