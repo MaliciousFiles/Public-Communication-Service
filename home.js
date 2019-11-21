4afc736353076d4d94215a0d5a1726d26feb54cf
Malcolm07 <57738870+Malcolm07@users.noreply.github.com>
2019-11-20 18:14:03 -0600

    2.1

--- home.js
+++ home.js
@@ -14,3 +14,30 @@ sleep(12000).then(() => {
       window.scrollTo(0,500);
     };
 });
+
+var slideIndex = 1
+showSlide(slideIndex)
+
+function changeSlide(n) {
+  showSlides(slideIndex += n);
+}
+
+function setSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("slide");
+  var dots = document.getElementsByClassName("slideDot");
+  if (n > slides.length) {slideIndex = 1}    
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";  
+  }
+  for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";  
+  dots[slideIndex-1].className += " active";
+}
