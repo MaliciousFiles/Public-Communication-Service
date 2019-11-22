90a06ad73880f56d78fe14f6aec7defc66ec3d2b
Malcolm07 <57738870+Malcolm07@users.noreply.github.com>
2019-11-22 09:49:52 -0600

    2.0.4

--- commWebLogin.js
+++ commWebLogin.js
@@ -32,7 +32,8 @@ function signUp() {
 		}
 	}
   //check username field
-  var usernameExistCheck = firebase.database().ref('/usernames').on("value", function(snapshot) {
+  var usernameExistCheck = firebase.database().ref('/usernames');
+  usernameExistCheck.on("value", function(snapshot) {
     usernameExistCheck = snapshot.val();
   });
   usernameExistCheck = checkEntry(usernameExistCheck, document.getElementById('username').value);
@@ -46,7 +47,8 @@ function signUp() {
     var usernameField = true;
   };
   //check password field
-  var passwordExistCheck = firebase.database().ref('/usernames').on("value", function(snapshot) {
+  var passwordExistCheck = firebase.database().ref('/usernames');
+  passwordExistCheck.on("value", function(snapshot) {
     passwordExistCheck = snapshot.val();
   });
   if (passwordExistCheck[document.getElementById('username').value]==hash(document.getElementById('password').value)) {
