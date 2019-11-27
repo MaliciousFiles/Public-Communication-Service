25f82bcb78a58538f9beded13d7fe8e35076d8df
Malcolm07 <57738870+Malcolm07@users.noreply.github.com>
2019-11-27 13:30:38 -0600

    3.5

--- commWebForgPass.js
+++ commWebForgPass.js
@@ -1,12 +1,11 @@
-function sendEmail () {
-	firebase.database().ref('/usernames/""').set("")
-	firebase.database().ref('/first names/""').set("")
-	firebase.database().ref('/last names/""').set("")
-	firebase.database().ref('/emails/""').set("")
-	firebase.database().ref('/passwords/""').set("")
-	firebase.database().ref('/profile images/""').set("")
-	firebase.database().ref('/reset tokens/""').set("")
-	function checkEntry(dict,entry) {
+function getUrlVars() {
+		var vars = {};
+		var parts = window.location.href.replace(/[?&]+([^=&]+)=([^&]*)/gi, function(m,key,value) {
+			vars[key] = value;
+		});
+		return vars;
+	}
+function checkEntry(dict,entry) {
 		for (index in Object.keys(dict)) {
 			if (dict[Object.keys(dict)[index]]==entry) {
 				var True=true
@@ -19,7 +18,26 @@ function sendEmail () {
 			}
 		
 	}
-	function getKeyByValue(object, value) {
+tokenCheck = firebase.database().ref('/reset tokens')
+tokenCheck.on('value', function(snapshot) {tokenCheck=snapshot.val()})
+if (checkEntry(tokenCheck, getUrlVars['token'])) {
+	document.getElementById('input').setAttribute('class', '')
+	document.getElementById('input').setAttribute('style', 'top: 5vh;left: 13%;position: fixed;padding: 20px;')
+	document.getElementById('input').innerHTML = '<center><h2 style="font-size: 150px;">Invalid code!</h2></center>'
+	document.getElementById('input').setAttribute('id', '');
+}
+function resetPass() {
+	function hash(string) { 
+                var hash = 0; 
+                if (string.length == 0) return hash; 
+                for (i = 0; i < string.length; i++) { 
+                    char = string.charCodeAt(i); 
+                    hash = ((hash << 5) - hash) + char; 
+                    hash = hash & hash; 
+                } 
+                return hash; 
+            }
+    function getKeyByValue(object, value) {
 	    for( var prop in object ) {
 		if( object.hasOwnProperty( prop ) ) {
 		     if( object[ prop ] === value )
@@ -27,75 +45,49 @@ function sendEmail () {
 		}
 	    }
 	}
-	function validateEmail(email) 
-	{
-		var re = /\S+@\S+\.\S+/;
-   		return re.test(email);
-	}
-	function makeToken() {
-	   var result = '';
-	   var characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
-	   var charactersLength = characters.length;
-	   for ( var i = 0; i < 10; i++ ) {
-	      result += characters.charAt(Math.floor(Math.random() * charactersLength));
-	   }
-	   return result;
+    function getUrlVars() {
+		var vars = {};
+		var parts = window.location.href.replace(/[?&]+([^=&]+)=([^&]*)/gi, function(m,key,value) {
+			vars[key] = value;
+		});
+		return vars;
 	}
 	function writeToDatabase(path,username,value) {
 		firebase.database().ref(path+'/'+username).set(value);
 	}
-  //check username field
-  var userExistCheck = firebase.database().ref('/usernames');
-  userExistCheck.on("value", function(snapshot) {
-    userExistCheck = snapshot.val();
-  });
-  userExistCheck = checkEntry(userExistCheck, document.getElementById('username').value);
-  var emailExistCheck = firebase.database().ref('/emails');
-  emailExistCheck.on("value", function(snapshot) {
-	  emailExistCheck = snapshot.val();
-  });
-  emailExistCheck = checkEntry(emailExistCheck, document.getElementById('username').value);
-  if (emailExistCheck || userExistCheck) {
-    var usernameExistCheck = true;
-  } else {
-    var usernameExistCheck = false;
-  }
-  //if statements
-  if (document.getElementById('username').value=="") {
-    document.getElementById('usernameError').innerHTML = "The username/email field can't be blank!"
-  } else if (usernameExistCheck==false) {
-    	document.getElementById('usernameError').innerHTML = "Username/email incorrect!";
-  } else {
-    document.getElementById('usernameError').innerHTML = ""
-    var usernameField = true;
-  };
-  if (usernameField==true) {
-	
-	var username = firebase.database().ref('/emails')
-	username.on("value", function(snapshot) {
-		username = snapshot.val();
-	});
-	username = getKeyByValue(username, document.getElementById('username').value);
-	if (username==null) {
-		username=document.getElementById('username').value
+	//get token
+	var token = getUrlVars()['token']
+	//password field
+	var passwordField = false
+	if (document.getElementById('password').value=="") {
+		document.getElementById('passwordError').innerHTML = "The password field can't be blank!"
+	} else {
+		document.getElementById('passwordError').innerHTML = ""
+		passwordField = true
 	};
-	var email = firebase.database().ref("/emails")
-	email.on('value', function(snapshot) {
-		email = snapshot.val();
-	})
-	email = email[document.getElementById("username").value];
-	if (email==null) {
-		email = document.getElementById('username').value;
+	//start checks for password check field
+	var passwordCheckField = false
+	//if statements
+	if (document.getElementById('passwordCheck').value=="") {
+		document.getElementById('passwordCheckError').innerHTML = "The password check field can't be blank"
+	} else if (document.getElementById('password').value!=document.getElementById('passwordCheck').value) {
+		document.getElementById('passwordCheckError').innerHTML = "Passwords don't match!"
+	} else {
+		document.getElementById('passwordCheckError').innerHTML = "";
+	};
+	if (document.getElementById('password').value!=document.getElementById('passwordCheck').value) {
+		document.getElementById('passwordCheckError').innerHTML = "Passwords don't match!"
+	} else {
+		document.getElementById('passwordCheckError').innerHTML = "";
+		passwordCheckField = true
+	};
+	if (passwordField==true && passwordCheckField==true) {
+		var username = firebase.database().ref('/reset tokens')
+		username.on('value', function(snapshot) {username=snapshot.val()})
+		username = getKeyByValue(username, token)
+		var password=hash(document.getElementById('password').value);
+		writeToDatabase('/passwords',username,password);
+		firebase.database().ref('/reset tokens/'+username).remove()
+		window.location.href = "./commWebLogin.html"
 	}
-	do {
-		var token = makeToken()
-		var tokenCheck = firebase.database().ref('/reset tokens')
-		tokenCheck.on("value", function(snapshot) {
-			tokenCheck = snapshot.val();
-		});
-	} while (checkEntry(tokenCheck, token));
-	writeToDatabase('/reset tokens', username, token)
-	emailjs.send("gmail", "forgot_password", {"to":email,"user":username,"token":token})
-  	window.location.href = "./commWebLogin.html"
-  }
 }
