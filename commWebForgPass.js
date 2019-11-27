5c6a0760883cff13c0ab24549487c76c08f82d05
Malcolm07 <57738870+Malcolm07@users.noreply.github.com>
2019-11-27 12:51:45 -0600

    3.4.1

--- commWebForgPass.js
+++ commWebForgPass.js
@@ -70,24 +70,7 @@ function sendEmail () {
     var usernameField = true;
   };
   if (usernameField==true) {
-	/*document.getElementById('username').setAttribute('type', 'password');
-	document.getElementById('username').setAttribute('placeholder', '');
-	document.getElementById('username').value = '';
-  	document.getElementById('username').setAttribute('id', 'password');
-	document.getElementById('email').setAttribute('type', 'password');
-	document.getElementById('email').setAttribute('placeholder', '');
-	document.getElementById('email').value = '';
-	document.getElementById('email').setAttribute('id', 'passwordCheck');
-	document.getElementById('usernameLabel').innerHTML = 'Password';
-	document.getElementById('usernameLabel').setAttribute('id', 'passwordLabel');
-	document.getElementById('emailLabel').innerHTML = 'Password Again';
-	document.getElementById('emailLabel').setAttribute('id', 'passwordCheckLabel');
-	document.getElementById('recoverButton').innerHTML = 'Set Password';
-	document.getElementById('recoverButton').setAttribute('onclick', 'setPassword()');
-	document.getElementById('recoverButton').setAttribute('id', 'setButton');
-	document.getElementById('usernameError').setAttribute('id', 'passwordError');
-	document.getElementById('emailError').setAttribute('id', 'passwordCheckError');
-	document.getElementById('forgPassTitle').innerHTML = "Reset Password";*/
+	
 	var username = firebase.database().ref('/emails')
 	username.on("value", function(snapshot) {
 		username = snapshot.val();
@@ -116,48 +99,3 @@ function sendEmail () {
   	window.location.href = "./commWebLogin.html"
   }
 }
-
-/*function setPassword() {
-	function hash(string) { 
-                var hash = 0; 
-                if (string.length == 0) return hash; 
-                for (i = 0; i < string.length; i++) { 
-                    char = string.charCodeAt(i); 
-                    hash = ((hash << 5) - hash) + char; 
-                    hash = hash & hash; 
-                } 
-                return hash; 
-            } 
-	function writeToDatabase(path,username,value) {
-		firebase.database().ref(path+'/'+username).set(value);
-	}
-	//password field
-	var passwordField = false
-	if (document.getElementById('password').value=="") {
-		document.getElementById('passwordError').innerHTML = "The password field can't be blank!"
-	} else {
-		document.getElementById('passwordError').innerHTML = ""
-		passwordField = true
-	};
-	//start checks for password check field
-	var passwordCheckField = false
-	//if statements
-	if (document.getElementById('passwordCheck').value=="") {
-		document.getElementById('passwordCheckError').innerHTML = "The password check field can't be blank"
-	} else if (document.getElementById('password').value!=document.getElementById('passwordCheck').value) {
-		document.getElementById('passwordCheckError').innerHTML = "Passwords don't match!"
-	} else {
-		document.getElementById('passwordCheckError').innerHTML = "";
-	};
-	if (document.getElementById('password').value!=document.getElementById('passwordCheck').value) {
-		document.getElementById('passwordCheckError').innerHTML = "Passwords don't match!"
-	} else {
-		document.getElementById('passwordCheckError').innerHTML = "";
-		passwordCheckField = true
-	};
-	if (passwordField==true && passwordCheckField==true) {
-		var password=hash(document.getElementById('password').value);
-		writeToDatabase('/passwords',window.username,password);
-		window.location.href = "./commWebLogin.html"
-	}
-}*/
