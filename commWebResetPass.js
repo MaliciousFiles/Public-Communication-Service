cf72ff2fbdd087cd1eef50772b1d60f4f02834be
Malcolm07 <57738870+Malcolm07@users.noreply.github.com>
2019-11-30 18:21:45 -0600

    1.3.10

--- commWebResetPass.js
+++ commWebResetPass.js
@@ -38,7 +38,7 @@ var tokenCheck = firebase.database().ref('/reset tokens')
 tokenCheck.on('value', function(snapshot) {
 	tokenDict=snapshot.val();
 })
-console.log(tokenDict);
+console.log(window.tokenDict);
 if (checkEntry(tokenDict, getUrlVars()['token'])==false) {
 	document.getElementById('input').setAttribute('class', '')
 	document.getElementById('input').setAttribute('style', 'top: 5vh;left: 13%;position: fixed;padding: 20px;')
