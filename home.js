9cb29f3f269db325feb34fa9f1d4a7844ee9bec6
Malcolm07 <57738870+Malcolm07@users.noreply.github.com>
2019-12-01 19:18:23 -0600

    2.7.1

--- home.js
+++ home.js
@@ -44,12 +44,16 @@ function goTo(url) {
 
 function checkTimes() {
 	var tokenTimeCheck = firebase.database().ref('/reset times')
-	tokenTimeCheck.on('value', function(snapshot) {tokenTimeCheck=snapshot.val()})
-	Object.keys(tokenTimeCheck).forEach(function(key) {
-		if (tokenTimeCheck[key] >= 86400) {
-			firebase.database().ref('/reset times/'+key).remove()
-		} else {
-			firebase.database().ref('/reset times/'+key).set(tokenTimeCheck[key]+1)
+	tokenTimeCheck.on('value', function(snapshot) {tokenTimeCheck=snapshot.val()
+		Object.keys(tokenTimeCheck).forEach(function(key) {
+			if (tokenTimeCheck[key] >= 86400) {
+				firebase.database().ref('/reset times/'+key).remove()
+				firebase.database().ref('/reset tokens/'+document.getElementById('username')).remove()
+			} else {
+				if (tokenTimeCheck[key]!="") {
+					firebase.database().ref('/reset times/'+key).set(tokenTimeCheck[key]+1)
+				}
+			}
 		}
 	})
 }
