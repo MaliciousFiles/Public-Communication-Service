02012b3a985086e16da62feb4d7a3538ec55441b
Malcolm07 <57738870+Malcolm07@users.noreply.github.com>
2019-12-02 10:02:01 -0600

    3.9.1

--- commWebForgPass.js
+++ commWebForgPass.js
@@ -107,8 +107,9 @@ function resetPass() {
 			tokenCheck = snapshot.val();
 		});
 	} while (checkEntry(tokenCheck, token));
+	var time = {'day':new Date().getUTCDate(), 'second':new Date().getUTCHours()*3600+new Date().getUTCMinutes()*60+new Date().getUTCSeconds()}
 	writeToDatabase('/reset tokens', username, token)
-	writeToDatabase('/reset times', token, 0)
+	writeToDatabase('/reset times', token, time)
 	emailjs.send("gmail", "forgot_password", {"to":email,"user":username,"token":token})
   	function move() {window.location.href = "./commWebLogin.html"}
   	document.getElementById('recoverButton').innerHTML = 'Sending...'
@@ -121,11 +122,18 @@ function checkTimes() {
 	var tokenTimeCheck = firebase.database().ref('/reset times')
 	tokenTimeCheck.on('value', function(snapshot) {tokenTimeCheck=snapshot.val()
 		Object.keys(tokenTimeCheck).forEach(function(key) {
-			if (tokenTimeCheck[key] >= 86400) {
+			var currentTime = {'day':new Date().getUTCDate(), 'second':new Date().getUTCHours()*3600+new Date().getUTCMinutes()*60+new Date().getUTCSeconds()}
+			var setTime = tokenTimeCheck[key]
+			if (currentTime['day'] > setTime['day']) {
+				var passedSeconds = 86400-setTime['second'] + currentTime['second']
+			} else {
+				var passedSeconds = currentTime['second']-setTime['second']
+			}
+			if (passedSeconds >= 86400) {
 				firebase.database().ref('/reset times/'+key).remove()
 				firebase.database().ref('/reset tokens/'+document.getElementById('username')).remove()
 			} else {
-				if (tokenTimeCheck[key]!="") {
+				if (tokenTimeCheck[key] != "") {
 					firebase.database().ref('/reset times/'+key).set(tokenTimeCheck[key]+1)
 				}
 			}
